3f423ab915fea790c7dea3d11113bee2b4fdef86
mayds1010 <mayds1010@gmail.com>
2023-02-14 16:19:17 +0900

    commit

--- src/main/java/com/project/final_retoree/controller/ByPassController.java
+++ src/main/java/com/project/final_retoree/controller/ByPassController.java
@@ -1,5 +1,20 @@
 package com.project.final_retoree.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ByPassController {
+    @RequestMapping(value = { "/main_search" })
+    public void forms() {
+        int i = 0;
+
+    }
+
+    @RequestMapping(value = { "/**" })
+    public void byPass() {
+        int i = 0;
+
+    }
 
 }
