ff3909665c2c907fd6acd13f2b831bcd445eee9f
seonbuzz <ddonsdevelop@gmail.com>
2023-02-16 01:15:23 +0900

    database

---
+++ DataBases/table_schemas/car_detail/car_detail_dml.sql
@@ -0,0 +1,18 @@
+INSERT INTO `retoree_FinalProject`.`CAR_DETAIL_INFO` (`CAR_DTL_UID`, `CAR_NUMBER`, `MANUFECTURER`, `MODEL`, `MODEL_YEAR`, `DRIVEN_DIST`, `TRANSMISSION`, `PRICE`, `DEALER_NAME`, `CELL_NUMBER`, `SALE`, `SOLD_OUT`, `ACCIDENT_INFO`)
+ VALUES ('1', '23 허 0101', '현대	', '그랜져 하이브리드	', '2020년 3월	', '70,123km', '자동(오토)', '2,000만원	', '홍길동	', '010-1234-1234', '20대	', '3대	', '무사고 차량	');
+ 
+INSERT INTO `retoree_FinalProject`.`CAR_DETAIL_INFO` (`CAR_DTL_UID`, `CAR_NUMBER`, `MANUFECTURER`, `MODEL`, `MODEL_YEAR`, `DRIVEN_DIST`, `TRANSMISSION`, `PRICE`, `DEALER_NAME`, `CELL_NUMBER`, `SALE`, `SOLD_OUT`, `ACCIDENT_INFO`)
+ VALUES ('2', '80아5959', '현대	', '코나 하이브리드	', '20년 8월	', '30,123km', '자동(오토)', '2,100만', '신사임당	', '010-5678-5678', '11대	', '5대	', '무사고 차량	');
+
+
+INSERT INTO `retoree_FinalProject`.`MAIN_SKELETON` (`SKELETON_UID`, `FRONT_PANEL`, `CROSS_MEMBER`, `INSIDE_PANEL`, `SIDE_MEMBER`, `WHEEL_HOUSE`, `PACKAGE_TRAY`, `DASH_PANEL`, `FLOOR_PANEL`, `FILER_PANEL`, `REAR_PANEL`, `TRUNK_FLOOR`) 
+VALUES ('1', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ');
+
+INSERT INTO `retoree_FinalProject`.`MAIN_SKELETON` (`SKELETON_UID`, `FRONT_PANEL`, `CROSS_MEMBER`, `INSIDE_PANEL`, `SIDE_MEMBER`, `WHEEL_HOUSE`, `PACKAGE_TRAY`, `DASH_PANEL`, `FLOOR_PANEL`, `FILER_PANEL`, `REAR_PANEL`, `TRUNK_FLOOR`) 
+VALUES ('2', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ');
+
+INSERT INTO `retoree_FinalProject`.`PANEL` (`PANEL_UID`, `HOOD`, `FRONT_FENDER`, `DOOR`, `SIDE_PANEL`, `TRUNK_LID`, `ROOF_PANEL`, `QUARTER_PANEL`)
+VALUES ('1', '정상 ', '교환 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ');
+
+INSERT INTO `retoree_FinalProject`.`PANEL` (`PANEL_UID`, `HOOD`, `FRONT_FENDER`, `DOOR`, `SIDE_PANEL`, `TRUNK_LID`, `ROOF_PANEL`, `QUARTER_PANEL`) 
+VALUES ('2', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ');
---
+++ DataBases/table_schemas/visit_reserve/visit_reserve_dml.sql
@@ -0,0 +1,5 @@
+INSERT INTO `retoree_FinalProject`.`VISIT_RESERVE` (`RESERVE_UID`, `STATUS`, `NAME`, `CELL_NUMBER`, `SALE`, `SOLD_OUT`, `STORE`, `TIME`, `PHONE`, `FAX`, `ADDRESS`, `MODEL`, `MODEL_YEAR`, `DRIVEN_DIST`, `PRICE`) 
+VALUES ('1', '확정', '홍길동', '010-1234-1234', '10대', '5대', 'KH 자동차', '', '02-1234-1234', '053-1234-1234', '서울 강남구 테헤란로14길 6 남도빌딩 KH 자동차', '현대 그랜저 하이브리드', '2018년식', '500km	', '2,250만원');
+
+INSERT INTO `retoree_FinalProject`.`VISIT_RESERVE` (`RESERVE_UID`, `STATUS`, `NAME`, `CELL_NUMBER`, `SALE`, `SOLD_OUT`, `STORE`, `TIME`, `PHONE`, `ADDRESS`, `MODEL`, `MODEL_YEAR`, `DRIVEN_DIST`, `PRICE`) 
+VALUES ('2', '확정', '신사임', '010-5678-5678', '13', '7', '강남제일', '9시~18', '02-5678-5678', '서울 강남구 테헨란로 13길 7 강남제일', '현대 KONA 하이브리', '2019년', '250km', '2,340만');
