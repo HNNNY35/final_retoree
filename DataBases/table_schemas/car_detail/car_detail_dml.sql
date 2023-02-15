c322ea6fa251c93546766abee3fac40348c56314
seonbuzz <ddonsdevelop@gmail.com>
2023-02-16 01:22:11 +0900

    database

--- DataBases/table_schemas/car_detail/car_detail_dml.sql
+++ DataBases/table_schemas/car_detail/car_detail_dml.sql
@@ -1,18 +1,18 @@
-INSERT INTO `retoree_FinalProject`.`CAR_DETAIL_INFO` (`CAR_DTL_UID`, `CAR_NUMBER`, `MANUFECTURER`, `MODEL`, `MODEL_YEAR`, `DRIVEN_DIST`, `TRANSMISSION`, `PRICE`, `DEALER_NAME`, `CELL_NUMBER`, `SALE`, `SOLD_OUT`, `ACCIDENT_INFO`)
- VALUES ('1', '23 허 0101', '현대	', '그랜져 하이브리드	', '2020년 3월	', '70,123km', '자동(오토)', '2,000만원	', '홍길동	', '010-1234-1234', '20대	', '3대	', '무사고 차량	');
+INSERT INTO `retoree_FinalProject`.`CAR_DETAIL_INFO` (`CAR_DTL_UID`, `CAR_NUMBER`, `MANUFECTURER`, `MODEL`, `MODEL_YEAR`, `DRIVEN_DIST`, `TRANSMISSION`, `PRICE`, `DTL_IMG_CAR`, `DEALER_NAME`, `CELL_NUMBER`, `SALE`, `SOLD_OUT`, `ACCIDENT_INFO`)
+ VALUES ('1', '23 허 0101', '현대', '그랜져 하이브리드', '2020년 3월', '70,123km', '자동(오토)', '2,000만원', 'https://cdn.top-rider.com/news/photo/202211/62761_144394_4518.jpg' '홍길동', '010-1234-1234', '20대', '3대', '무사고 차량');
  
-INSERT INTO `retoree_FinalProject`.`CAR_DETAIL_INFO` (`CAR_DTL_UID`, `CAR_NUMBER`, `MANUFECTURER`, `MODEL`, `MODEL_YEAR`, `DRIVEN_DIST`, `TRANSMISSION`, `PRICE`, `DEALER_NAME`, `CELL_NUMBER`, `SALE`, `SOLD_OUT`, `ACCIDENT_INFO`)
- VALUES ('2', '80아5959', '현대	', '코나 하이브리드	', '20년 8월	', '30,123km', '자동(오토)', '2,100만', '신사임당	', '010-5678-5678', '11대	', '5대	', '무사고 차량	');
+INSERT INTO `retoree_FinalProject`.`CAR_DETAIL_INFO` (`CAR_DTL_UID`, `CAR_NUMBER`, `MANUFECTURER`, `MODEL`, `MODEL_YEAR`, `DRIVEN_DIST`, `TRANSMISSION`, `PRICE`, `DTL_IMG_CAR`, `DEALER_NAME`, `CELL_NUMBER`, `SALE`, `SOLD_OUT`, `ACCIDENT_INFO`)
+ VALUES ('2', '80아5959', '현대', '코나 하이브리드', '20년 8월', '30,123km', '자동(오토)', '2,100만','http://file.mk.co.kr/meet/neds/2022/05/image_readmed_2022_414468_16522268155038384.jpg', '신사임당', '010-5678-5678', '11대', '5대', '무사고 차량');
 
 
 INSERT INTO `retoree_FinalProject`.`MAIN_SKELETON` (`SKELETON_UID`, `FRONT_PANEL`, `CROSS_MEMBER`, `INSIDE_PANEL`, `SIDE_MEMBER`, `WHEEL_HOUSE`, `PACKAGE_TRAY`, `DASH_PANEL`, `FLOOR_PANEL`, `FILER_PANEL`, `REAR_PANEL`, `TRUNK_FLOOR`) 
-VALUES ('1', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ');
+VALUES ('1', '정상', '정상', '정상', '정상', '정상', '정상', '정상', '정상', '정상', '정상', '정상');
 
 INSERT INTO `retoree_FinalProject`.`MAIN_SKELETON` (`SKELETON_UID`, `FRONT_PANEL`, `CROSS_MEMBER`, `INSIDE_PANEL`, `SIDE_MEMBER`, `WHEEL_HOUSE`, `PACKAGE_TRAY`, `DASH_PANEL`, `FLOOR_PANEL`, `FILER_PANEL`, `REAR_PANEL`, `TRUNK_FLOOR`) 
-VALUES ('2', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ');
+VALUES ('2', '정상', '정상', '정상', '정상', '정상', '정상', '정상', '정상', '정상', '정상', '정상');
 
 INSERT INTO `retoree_FinalProject`.`PANEL` (`PANEL_UID`, `HOOD`, `FRONT_FENDER`, `DOOR`, `SIDE_PANEL`, `TRUNK_LID`, `ROOF_PANEL`, `QUARTER_PANEL`)
-VALUES ('1', '정상 ', '교환 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ');
+VALUES ('1', '정상', '교환', '정상', '정상', '정상', '정상', '정상');
 
 INSERT INTO `retoree_FinalProject`.`PANEL` (`PANEL_UID`, `HOOD`, `FRONT_FENDER`, `DOOR`, `SIDE_PANEL`, `TRUNK_LID`, `ROOF_PANEL`, `QUARTER_PANEL`) 
-VALUES ('2', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ', '정상 ');
+VALUES ('2', '정상', '정상', '정상', '정상', '정상', '정상', '정상');
