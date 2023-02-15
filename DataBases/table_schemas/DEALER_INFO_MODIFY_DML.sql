998b19c3efb0f51bde07141742afb310ee010189
HNNNY35 <hnnny35@gmail.com>
2023-02-15 14:20:06 +0900

    erd

---
+++ DataBases/table_schemas/DEALER_INFO_MODIFY_DML.sql
@@ -0,0 +1,6 @@
+INSERT INTO DEALER_INFO_MODIFY
+(UNIQUE_ID, PROFILE_IMG, NANE, BR_NUMBER, ID, PASSWORD, CELL_NUMBER, EMAIL, ADDR_CITY, ADDR_DISTRICT)
+VALUES
+('D01', '../image/profile.jpg', '양파쿵야', '111-11-11111', 'ky_restaurant', 'a123456789', '01011112222', 'ky_restaurant@naver.com', '서울시', '강남구'),
+('D02', '../image/profile.jpg', '김핸', '222-11-11111', 'hihi', 'b123456789', '01022223333', 'hihi@naver.com', '서울시', '동작구')
+;
