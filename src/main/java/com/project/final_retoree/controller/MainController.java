a04b10e398367d983471dc02d29a968d84972dac
mayds1010 <mayds1010@gmail.com>
2023-02-15 15:44:47 +0900

    main

--- src/main/java/com/project/final_retoree/controller/MainController.java
+++ src/main/java/com/project/final_retoree/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
 
-    @RequestMapping(value = "/main_search")
+    @RequestMapping(value = { "", "/", "/main_search" })
     public ModelAndView signup(ModelAndView modelAndView) {
         modelAndView.setViewName("/main_search");
         return modelAndView;
