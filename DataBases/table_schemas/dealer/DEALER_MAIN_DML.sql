e533c8846631d1480fa8a3773370affe441da959
HNNNY35 <hnnny35@gmail.com>
2023-02-16 09:28:57 +0900

    dealer

---
+++ DataBases/table_schemas/dealer/DEALER_MAIN_DML.sql
@@ -0,0 +1,6 @@
+INSERT INTO DEALER_MAIN
+(UNIQUE_ID,  NAME, CELL_NUMBER, EMAIL, ADDR_CITY, ADDR_DISTRICT, DETAILED_MODEL, VEH_NUM, DTL_IMG_CAR, PRICE, QUESTION_TITLE, USERID, QUESTION_DATE)
+VALUES
+('D01', '김양파', '01011112222', 'ky_restaurant@naver.com', '서울시', '강남구', '그랜져 하이브리드', '23허0101', 'grandeur.png', '2000', '할부문의 드립니다', 'asdf', '2023-02-03'),
+('D02', '이쿵야', '01022223333', 'hihi@naver.com', '서울시', '동작구', '그랜져 하이브리드', '25아1231', 'grandeur.png', '2100', '구매문의', 'qwerty', '2023-02-01')
+;
