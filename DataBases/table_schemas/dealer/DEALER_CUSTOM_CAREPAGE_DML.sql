11a8e4b2e6df9a09d4bd086b7e94a7f69ddf5ad1
HNNNY35 <hnnny35@gmail.com>
2023-02-16 10:15:03 +0900

    dealer

---
+++ DataBases/table_schemas/dealer/DEALER_CUSTOM_CAREPAGE_DML.sql
@@ -0,0 +1,6 @@
+INSERT INTO DEALER_CUSTOM_CAREPAGE
+(UNIQUE_ID, INQ_TITLE, INQ_CONTENT, USER_ID, INQ_DATE, ANS_CONTENT, ANS_DATE, ANS_STATUS)
+VALUES
+('Q01', '할부 문의', '할부 문의 드립니다. ㅁㅁ카드로 구매할 건데...', 'qwerty1234', '2023-02-15', '안녕하세요, 고객님. 유선 상담 진행하였습니다.', '2023-02-16', '답변완료'),
+('Q02', '방문상담 예약', '방문상담 예약하고 싶습니다. 그냥 찾아가면 되나요?', 'ele111', '2023-02-11', '안녕하세요, 고객님. 사이트 내 방문상담 예약 기능 이용하여 예약 부탁드립니다.', '2023-02-16', '답변완료')
+;
