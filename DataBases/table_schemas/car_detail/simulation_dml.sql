c1a9b3a657598ead040acc9a8f9902fd36ca53b9
seonbuzz <ddonsdevelop@gmail.com>
2023-02-16 09:46:56 +0900

    db

--- DataBases/table_schemas/car_detail/simulation_dml.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `retoree_FinalProject`.`SIMULATION` (`uid`, `vehicle_price`, `registration_tax`, `acquisition_tax`, `public_purchase_cost`, `increase_rent`, `stamp_duty`, `total_price`) 
-VALUES ('1', '20,000,000원', '800,000', '360,000', '213,000', '1,000', '3,000', '23,201,000');
-
-INSERT INTO `retoree_FinalProject`.`SIMULATION` (`uid`, `vehicle_price`, `registration_tax`, `acquisition_tax`, `public_purchase_cost`, `increase_rent`, `stamp_duty`, `total_price`) 
-VALUES ('2', '21,000,000', '850,000', '420,000', '230,000', '1,000', '4,000', '21,205,000');
