81f7af4e7223f66a3537adece827da571f6f060a
seonbuzz <ddonsdevelop@gmail.com>
2023-02-16 10:43:14 +0900

    db

---
+++ DataBases/table_schemas/mypage/MYPAGE_MODIFY_DML.sql
@@ -0,0 +1,6 @@
+INSERT INTO MYPAGE_MODIFY
+(USER_UID, NAME, BIRTHDAY,ID,PASSWORD, CELL_NUMBER, EMAIL, ADDR_CITY, ADDR_DISTRICT)
+VALUES
+('1', '김우디','1994-01-01','woodieboogie','woody','010-5959-5858','woody@woody.com','서울특별시 강남구', '테헤란로 6길 13'),
+('2', '이서울','1997-05-09','iseoulu','soullee','010-4040-1010','soul@seoul.com','서울특별시 강남구', '테헤란로 7길 68')
+;
