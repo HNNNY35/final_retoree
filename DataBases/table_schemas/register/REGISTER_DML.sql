8e8fafba1c796dee5c8c64add76c2bb577a92e03
mayds1010 <mayds1010@gmail.com>
2023-02-21 19:44:47 +0900

    register

--- DataBases/table_schemas/register/REGISTER_DML.sql
+++ DataBases/table_schemas/register/REGISTER_DML.sql
@@ -5,4 +5,6 @@ VALUES
 ('U02', '송혜교', 'hyekyo1122', 'hk1122' , '19811122' , '01012341234', 'song_hyekyo@gmail.com', '서울시', '용산구' , 'YES' , 'NO')
 ; 
 
-SELECT * FROM REGISTER_2;
+SELECT USER_ID, NAME, ID, PASSWORD, BIRTHDAY, CELL_NUMBER, EMAIL, ADDR_CITY, ADDR_DISTRICT 
+FROM REGISTER_2
+;
