9b8669cb10df7671f480a34f5992e16629daf074
mayds1010 <mayds1010@gmail.com>
2023-02-22 13:40:32 +0900

    register

--- src/main/java/com/project/final_retoree/services/RegisterService.java
+++ src/main/java/com/project/final_retoree/services/RegisterService.java
@@ -30,16 +30,16 @@ public class RegisterService {
         return result;
     }
 
-    public Object register(Object dataMap) {
-        Object result = this.insertregister(dataMap);
-        result = this.insertagree(dataMap);
-        return result;
-    }
+    // public Object register(Object dataMap) {
+    // Object result = this.insertregister(dataMap);
+    // result = this.insertagree(dataMap);
+    // return result;
+    // }
 
-    public Object select(Object dataMap) {
-        String sqlMapId = "Register.selectFromRegister";
-        Object result = registerDao.getOne(sqlMapId, dataMap);
-        return result;
-    }
+    // public Object select(Object dataMap) {
+    // String sqlMapId = "Register.selectFromRegister";
+    // Object result = registerDao.getOne(sqlMapId, dataMap);
+    // return result;
+    // }
 
 }
