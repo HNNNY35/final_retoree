70de99736dcc81412f317e7c4d70d30308d6e5ba
seonbuzz <ddonsdevelop@gmail.com>
2023-02-23 11:44:10 +0900

    CarInfo

--- src/main/java/com/project/final_retoree/bean/CarInfo.java
+++ src/main/java/com/project/final_retoree/bean/CarInfo.java
@@ -1,5 +1,9 @@
 package com.project.final_retoree.bean;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class CarInfo {
 
     private String car_id;
