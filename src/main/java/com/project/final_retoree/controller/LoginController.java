30ea6739166829e935800003751c3950cf4cf6a7
mayds1010 <mayds1010@gmail.com>
2023-02-28 14:02:14 +0900

    login

---
+++ src/main/java/com/project/final_retoree/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.project.final_retoree.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    // 로그인
+    @RequestMapping(value = "/login")
+    public ModelAndView login(ModelAndView modelAndView) {
+        modelAndView.setViewName("login_register/login");
+        return modelAndView;
+    }
+
+}
